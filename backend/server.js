13b5d10c50c81d90762f4da9d8c10e7912cc35ed
황종완 <75131851+hjw1191@users.noreply.github.com>
2024-08-24 00:26:19 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app = express();
-const { MongoClient, ObjectId } = require('mongodb'); // ObjectId 추가
+const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors');
 
+const app = express();
 app.use(cors()); // CORS 문제 해결
 app.use(express.static(__dirname + '/public')); // 정적 파일 제공
 app.set('view engine', 'ejs'); // 템플릿 엔진 설정
 
-const url = 'mongodb+srv://hjw1191:zxc123@hjw1191.zaqklsn.mongodb.net/?retryWrites=true&w=majority&appName=hjw1191'; 
+const url = 'mongodb+srv://hjw1191:zxc123@hjw1191.zaqklsn.mongodb.net/?retryWrites=true&w=majority&appName=hjw1191';
 const client = new MongoClient(url);
 
 client.connect()
@@ -29,30 +29,28 @@ client.connect()
 
     // 특정 드라마 ID로 데이터를 가져오는 엔드포인트
     app.get('/api/dramas/:id', async (req, res) => {
-  try {
-    const dramaId = req.params.id;
-    if (!ObjectId.isValid(dramaId)) {
-      return res.status(400).send('잘못된 드라마 ID 형식입니다.');
-    }
+      try {
+        const dramaId = req.params.id;
+        if (!ObjectId.isValid(dramaId)) {
+          return res.status(400).send('잘못된 드라마 ID 형식입니다.');
+        }
 
-    const drama = await linkCollection.findOne({ _id: new ObjectId(dramaId) });
-    if (drama) {
-      res.json(drama);
-    } else {
-      res.status(404).send('드라마를 찾을 수 없습니다.');
-    }
-  } catch (err) {
-    console.error('DB 조회 중 오류 발생:', err);
-    res.status(500).send('DB 조회 중 오류 발생');
-  }
-});
-
-
-    app.listen(8080, () => {
-      console.log('http://localhost:8080 에서 서버 실행 중');
+        const drama = await linkCollection.findOne({ _id: new ObjectId(dramaId) });
+        if (drama) {
+          res.json(drama);
+        } else {
+          res.status(404).send('드라마를 찾을 수 없습니다.');
+        }
+      } catch (err) {
+        console.error('DB 조회 중 오류 발생:', err);
+        res.status(500).send('DB 조회 중 오류 발생');
+      }
     });
 
   })
   .catch((err) => {
     console.error('DB 연결 실패:', err);
   });
+
+// Vercel에서 이 모듈을 내보내기
+module.exports = app;
