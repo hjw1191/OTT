b6ec5071f1459150e5ba970a9bc5d8f7ba370970
황종완 <75131851+hjw1191@users.noreply.github.com>
2024-08-29 11:39:07 +0900

    로그인

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Title from './pages/Title';
+import Navbar from './components/Navbar';
+import Login from './components/Login';
 import D_infor from './pages/D_infor';
 import Movies from './pages/Movies';
 import MovieDetail from './pages/MovieDetail';
 import './styles/App.css';
 
-
 function App() {
+  const [isLoginVisible, setIsLoginVisible] = useState(false); // 로그인 창 가시성 상태
+
+  const openLogin = () => setIsLoginVisible(true);  // 로그인 창 열기
+  const closeLogin = () => setIsLoginVisible(false); // 로그인 창 닫기
+
   return (
     <Router>
+      <Navbar openLogin={openLogin} /> {/* openLogin 함수 전달 */}
+      {isLoginVisible && <Login onClose={closeLogin} />} {/* Login 컴포넌트 렌더링 */}
       <Routes>
         <Route path="/" element={<Title />} />
         <Route path="/D_infor/:id" element={<D_infor />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movie-detail" element={<MovieDetail />} /> 
+        <Route path="/movie-detail" element={<MovieDetail />} />
       </Routes>
     </Router>
   );
