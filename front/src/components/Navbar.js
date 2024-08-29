52d4cd773678633e323fe002478d1e20ee8b77fe
황종완 <75131851+hjw1191@users.noreply.github.com>
2024-08-29 11:35:00 +0900

    로그인활성화

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,38 +1,22 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/Navbar.css';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({ onSearch }) => {
+const Navbar = ({ openLogin }) => {
   const [query, setQuery] = useState('');
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    if (query) {
-      onSearch(query); // 부모 컴포넌트로 검색어를 전달하여 검색 수행
-    }
-  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">LINK</a>
+      <Link className="navbar-brand" to="/">LINK</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        
-        <form className="form-inline my-2 my-lg-0" onSubmit={handleSearch}>
-          <input
-            className="form-control mr-sm-2"
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Search for movies..."
-          />
-        
-        </form>
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <a className="nav-link" href="/login">Login</a>
+            {/* 버튼 클릭 시 openLogin 함수 호출 */}
+            <button className="nav-link btn btn-link" onClick={openLogin}>Login</button>
           </li>
         </ul>
       </div>
