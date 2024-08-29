98776b84ff2d0758b9f7e33ba7a03d574633a59f
황종완 <75131851+hjw1191@users.noreply.github.com>
2024-08-29 22:22:21 +0900

    Update D_infor.js

--- front/src/pages/D_infor.js
+++ front/src/pages/D_infor.js
@@ -12,7 +12,7 @@ function D_infor() {
 
   useEffect(() => {
     if (id) {
-      fetch(`https://port-0-ott-m09fpvc47fc6ed1f.sel4.cloudtype.app//api/dramas/${id}`)
+      fetch(`https://port-0-ott-m09fpvc47fc6ed1f.sel4.cloudtype.app/api/dramas/${id}`)
         .then(response => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
